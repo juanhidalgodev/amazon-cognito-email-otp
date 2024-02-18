9c60851694818ca2b10d51e67269186b2275ed50
Juan Hidalgo <jmhidalgoa@outlook.com>
2024-02-18 05:45:42 -0500

    resolve migration conflicts (no changing lambda triggers)

--- src/auth.js
+++ src/auth.js
@@ -1,63 +1,41 @@
-import { Auth } from '@aws-amplify/auth';
-import { Amplify } from '@aws-amplify/core';
-import { Logger } from 'aws-amplify';
+import { signIn, confirmSignIn, signOut } from 'aws-amplify/auth';
+import { Amplify } from 'aws-amplify';
+import { ConsoleLogger } from 'aws-amplify/utils';
+import config from './amplifyConfiguration'; 
 
-const logger = new Logger('ErrorLogger');
 
-/**
- * Sign in a user with the provided username and password.
- * @param {string} usr - The username of the user.
- * @param {string} pss - The password of the user.
- * @returns {Promise<object>} - A promise that resolves to the signed-in user object.
- */
-async function signIn(usr, pss) {
-  let amplifyConfig;
+const logger = new ConsoleLogger('ErrorLogger');
 
-  amplifyConfig = {
-    Auth: {
-      region: process.env.AWS_REGION,
-      userPoolId: process.env.USER_POOL_ID,
-      userPoolWebClientId: process.env.USER_POOL_WEB_CLIENT_ID,
-      mandatorySignIn: true,
-    },
-    authenticationFlowType: "USER_SRP_AUTH",
-  };
+async function amplifySignIn(username, password) {
 
-  Amplify.configure(amplifyConfig);
-
-  Auth.configure();
-
-  let user;
+  Amplify.configure(config);
 
   try {
-    await Auth.signIn(usr, pss);
 
-    amplifyConfig.authenticationFlowType = "CUSTOM_AUTH";
-    Amplify.configure(amplifyConfig);
-    Auth.configure();
+    await signIn({
+      username,
+      password,
+    });
 
-    user = await Auth.signIn(usr);
+    await signOut();
 
-    return user;
+    const { nextStep } = await signIn({
+      username,
+      options: {
+        authFlowType: 'CUSTOM_WITHOUT_SRP',
+      },
+    });
+
+    return nextStep;
   } catch (e) {
     logger.error('Is something wrong with the Sign In?',e);
   }
 }
 
-/**
- * Verifies the OTP (One-Time Password) for a user.
- * @param {object} user - The user object.
- * @param {string} code - The OTP code to verify.
- * @returns {Promise<any>} - A promise that resolves with the challenge answer response.
- */
-async function verifyOTP(user, code) {
-  if (user?.challengeName === "CUSTOM_CHALLENGE") {
+async function verifyOTP(nextStep, challengeResponse) {
+  if (nextStep?.signInStep === "CONFIRM_SIGN_IN_WITH_CUSTOM_CHALLENGE") {
     try {
-      const challengeAnswerResponse = await Auth.sendCustomChallengeAnswer(
-        user,
-        code
-      );
-
+      const challengeAnswerResponse = await confirmSignIn({ challengeResponse });
       return challengeAnswerResponse;
     } catch (e) {
       logger.error('Is something wrong with the verification of the OTP?', e);
@@ -65,4 +43,16 @@ async function verifyOTP(user, code) {
   }
 }
 
-export { signIn, verifyOTP };
+async function amplifySignOut(){
+
+  Amplify.configure(config);
+  
+  try {
+    await signOut();
+  } catch (e) {
+    logger.error('Is something wrong with the Sign Out?', e);
+  }
+}
+
+
+export { amplifySignIn, verifyOTP, amplifySignOut };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
-import { signIn, verifyOTP } from "./auth.js";
+import { amplifySignIn, verifyOTP, amplifySignOut} from "./auth.js";
 
-let user;
+let nextStep;
 
 const signInBtn = document.getElementById("signIn");
 signInBtn.addEventListener("click", async (event) => {
-  signInBtn.disabled = true;
+  //signInBtn.disabled = true;
   signInBtn.innerHTML = "Loading...";
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-  user = await signIn(username, password);
-  if (!user) {
+  nextStep = await amplifySignIn(username, password);
+  if (!nextStep) {
     signInBtn.innerHTML = "ERROR...";
     return;
   } else {
     signInBtn.innerHTML = "Sending OTP";
-    console.log("signIn :: ", user);
+    console.log("signIn :: ", nextStep);
     return;
   }
 });
 
 const sendOTPBtn = document.getElementById("sendOTP");
 sendOTPBtn.addEventListener("click", async (event) => {
-  sendOTPBtn.disabled = true;
+  //sendOTPBtn.disabled = true;
   sendOTPBtn.innerHTML = "Loading...";
-  const code = document.getElementById("codeOTP").value;
-  const verify = await verifyOTP(user, code);
-  if (!verify) {
+  const code = document.getElementById("OTP").value;
+  const response = await verifyOTP(nextStep, code);
+  if (!response) {
     sendOTPBtn.innerHTML = "ERROR...";
     return;
   } else {
     sendOTPBtn.innerHTML = "Verified";
-    console.log("verifyOTP :: ", verify);
+    console.log("result :: ", response);
     return;
   }
+});
+
+const signOutBtn = document.getElementById("signOut");
+signOutBtn.addEventListener("click", async (event) => {
+  signOutBtn.innerHTML = "Loading...";
+  await amplifySignOut();
+  signOutBtn.innerHTML = "You signed out";
 });
